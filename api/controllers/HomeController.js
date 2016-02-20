76dde40c776a9173aee9f84356e439d4e2400e04
agsantos <agsantos@voyagerinnovation.com>
2016-02-20 19:21:30 +0800

    Added Home controller and routes

---
+++ api/controllers/HomeController.js
@@ -0,0 +1,16 @@
+/**
+ * HomeController
+ *
+ * @description :: Server-side logic for managing Homes
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+module.exports = {
+
+  index: function(req, res) {
+    res.view();
+  },
+
+  login: function(req, res) {
+    res.view();
+  }
+};
