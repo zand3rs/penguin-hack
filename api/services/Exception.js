6dda83f185166fc46b814cb1ec90e218e848cec9
agsantos <agsantos@voyagerinnovation.com>
2016-02-20 18:52:44 +0800

    setup: added Exception Service

---
+++ api/services/Exception.js
@@ -0,0 +1,22 @@
+/*
+ * Exception
+ *
+ */
+
+var stderror = require("stderror");
+var Exception = stderror.extend("Exception");
+
+//==============================================================================
+//-- define exceptions here...
+
+Exception.define({name: "UnknownError", message: "Unknown error"});
+Exception.define({name: "SystemError", message: "System error"});
+Exception.define({name: "Forbidden", message: "Forbidden"});
+Exception.define({name: "ValidationError", message: "Validation error"});
+Exception.define({name: "InternalServerError", message: "Internal server error"});
+Exception.define({name: "InvalidCSRF", message: "Invalid CSRF"});
+
+//==============================================================================
+//-- exports
+
+module.exports = Exception;
