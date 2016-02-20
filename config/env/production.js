1159a366bcc15900ede4f99d2a0e53010cd0eb58
zander <aamagtipon@voyagerinnovation.com>
2016-02-20 23:48:17 +0800

    production setup

--- config/env/production.js
+++ config/env/production.js
@@ -12,27 +12,7 @@
 
 module.exports = {
 
-  /***************************************************************************
-   * Set the default database connection for models in the production        *
-   * environment (see config/connections.js and config/models.js )           *
-   ***************************************************************************/
-
-  // models: {
-  //   connection: 'someMysqlServer'
-  // },
-
-  /***************************************************************************
-   * Set the port in the production environment to 80                        *
-   ***************************************************************************/
-
-  // port: 80,
-
-  /***************************************************************************
-   * Set the log level in production environment to "silent"                 *
-   ***************************************************************************/
-
-  // log: {
-  //   level: "silent"
-  // }
+  host: "penguin.koding.io",
+  port: 8080
 
 };
