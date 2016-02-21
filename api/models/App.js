826dafdd411b463f9e2c6224cf536fe00a592ea7
agsantos <agsantos@voyagerinnovation.com>
2016-02-21 23:04:42 +0800

    Added aftercreate on App model

--- api/models/App.js
+++ api/models/App.js
@@ -25,6 +25,66 @@ module.exports = {
       type: "datetime",
       columnName: "updated_at"
     }
+  },
+
+  //-- lifecycle callbacks
+  afterCreate: function(values, next) {
+    var appId = values.id;
+
+    var editor = {
+      appId: appId,
+      name: "editor",
+      description: "editor",
+      permission: {
+        draft: {
+          canEdit: true,
+          canView: true
+        },
+        submitted: {
+          canDelete: true,
+          canArchive: true
+        }
+      },
+      manageModels: true,
+      manageImages: true
+    };
+
+    var contributor = {
+      appId: appId,
+      name: "contributor",
+      description: "contributor",
+      permission: {
+        draft: {
+          canAdd: true,
+          canEdit: true,
+          canView: true,
+          canDelete: true
+        },
+        submitted: {
+          canView: true
+        }
+      },
+      manageImages: true
+    };
+
+    var publisher = {
+      appId: appId,
+      name: "publisher",
+      description: "publisher",
+      permission: {
+        submitted: {
+          canView: true,
+          canPublish: true
+        },
+        published: {
+          canArchive: true
+        }
+      }
+    };
+
+    var roles = [editor, contributor, publisher];
+
+    Role.create(roles, next);
   }
 
 };
