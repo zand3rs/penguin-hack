00fda73cc96f16f3d4edbf67bff37a4c2cdd8454
zander <aamagtipon@voyagerinnovation.com>
2016-02-22 01:49:10 +0800

    updated prod callback url for google auth

--- config/env/production.js
+++ config/env/production.js
@@ -17,7 +17,7 @@ module.exports = {
   google: {
     key: "688356610593-vt2h2a3lsd0g7beh4fdboq1nuhrr1ipi.apps.googleusercontent.com",
     secret: "D8zYy2ddORfgBYf6w2ahH-xM",
-    callbackUrl: "http://169.45.108.228:8080/authorize"
+    callbackUrl: "http://uzks21d24582.zand3rs.koding.io:8080/authorize"
   }
 
 };
